9e52008a3d8e4c6b44bb5cce93ba8e90a1068c64
SanttosDev <josuels@id.uff.br>
2024-08-23 08:41:12 -0300

    Controle de Erro

--- script.js
+++ script.js
@@ -5,24 +5,31 @@ const Isenha = document.querySelector(".senha");
 const Icel = document.querySelector(".cel");
 
 function cadastrar() {
-    
-    fetch("http://localhos:8080/cadastrar",
-        {
-            headers: {
-                'Accept': 'aplication/json',
-                'Content-Type': 'aplication/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                nome: Inome.value,
-                email: Iemail.value,
-                senha: Isenha.value,
-                celular: Icel.value
-            })
+    fetch("http://localhost:8080/usuarios", {
+        headers: {
+            'Accept': 'application/json', 
+            'Content-Type': 'application/json' 
+        },
+        method: "POST",
+        body: JSON.stringify({
+            nome: Inome.value,
+            email: Iemail.value,
+            senha: Isenha.value,
+            celular: Icel.value 
         })
-        .then(function (res) {console.log(res) })
-        .catch(function (res) {console.log(res) })
-    
+    })
+    .then(function (res) {
+        if (res.ok) {
+            return res.json(); 
+        }
+        throw new Error('Resposta da rede não estava ok.');
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+        console.error('Houve um problema com a operação de fetch:', error);
+    });
 }
 
 function limpar() {
@@ -30,12 +37,11 @@ function limpar() {
     Iemail.value = "";
     Isenha.value = "";
     Icel.value = "";
-};
+}
 
 formulario.addEventListener("submit", function (event) {
     event.preventDefault();
 
     cadastrar();
     limpar();
-
 });
